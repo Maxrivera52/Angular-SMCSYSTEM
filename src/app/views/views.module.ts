9e8cefc0d68fb8299b4ecbd1265175fe5ce88c71
Max Rivera <maxrivera938@gmail.com>
2021-11-22 17:43:33 -0500

    template implemented successfully -no bs4-

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 
 @NgModule({
   declarations: [
   
     LoginComponent,
-       HomeComponent
+       HomeComponent,
+       TopBarComponent,
+       SideBarComponent,
   ],
   imports: [
     CommonModule
