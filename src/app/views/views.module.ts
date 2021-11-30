797c2bb90f6d90a3b9cff1e3ddd065f243db163d
Franco2598 <franpalomino2504@gmail.com>
2021-11-30 00:39:00 -0500

    crudaluversion2.3

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -14,6 +14,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
+
 @NgModule({
   declarations: [
     LoginComponent,
